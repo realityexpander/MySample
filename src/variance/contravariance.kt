7628c47c746146283323a9e855a26d09d4b931d1
realityexpander <realityexpander@gmail.com>
2019-10-24 21:24:23 -0500

    Add fun1 & fun2 alternates

--- src/variance/contravariance.kt
+++ src/variance/contravariance.kt
@@ -31,82 +31,91 @@ fun mainContravariance() {
 //        box.showItems()
 //    }
 
-    // using contravariance
-    val boxOfA: Box<A> = Box<A>()
-    val boxOfBPtrToBoxOfA: Box<B> = boxOfA
-    val boxOfB: Box<B> = Box<A>()
 
     open class AExecutor {
-        //open fun execute(arg: B) {}
-
-//        // This will *not* accept A // lambda
-//        open val execute: (B) -> Unit = {
-//            println("AExecutor execute (B) -> Unit")
-//            println("b=${it.b}")
+//        open fun execute(b: B) {
+//            println("AExecutor execute(b: B) b=${b.b}")
 //        }
 
-//        // Only accept type: B  // fun1
+        // lambda property (variable in an object is called a property)
+        // This will *not* accept A
+        open val execute: (B) -> Unit = {
+            println("AExecutor execute (B) -> Unit b=${it.b}")
+        }
+
+//        // fun1
+//        // Only accept type: B
 //        fun execute(b: B) {
 //            println("AExecutor execute(b: B)")
 //            println("b=${b.b}")
 //        }
 
-        open fun <T : B> execute(b: T) { // fun2
-            println("AExecutor execute(b: <T:B>) b=${b.b}")
-        }
+//        // fun2
+//        open fun <T : B> execute(b: T) {
+//            println("AExecutor execute(b: <T:B>) b=${b.b}")
+//        }
     }
 
     class BExecutor : AExecutor() {
-        //override fun execute(arg: A) {}
-
-//        // This will also accept B, bc B a subclass of A // lambda
-//        override val execute: (A) -> Unit = {
-//            println("BExecutor execute (A) -> Unit")
-//            println("a=${it.a}")
-//
-//            if(it is B) {
-//                println("(A) is really type B, b=${it.b}")
-//                super.execute(it as B)
-//            }
+//        fun execute(a: A) {
+//            println("BExecutor execute(a: A) a=${a.a}")
 //        }
 
-        // Only Accept type A // fun1
-        fun execute(a: A) {
-            println("BExecutor execute(a: A) a=${a.a}")
+        // lambda
+        // This will also accept B, bc B passed in is a subclass of A
+        override val execute: (A) -> Unit = {
+            println("BExecutor execute (A) -> Unit a=${it.a}")
+
+            // possible to process just A type info, but can also process for type B too.
+            if(it is B) {
+                println("  (A) is really type B tho, b=${it.b}")
+//                super.execute(it as B) // alt behavior, process with DRY in super
+            }
         }
 
-        // Only accept type B or subtype
-        override fun <T : B> execute(b: T) {  // fun2
-//        fun execute(b: B) {  // fun2
-            println("* BExecutor B -> AExecutor super.execute ")
-            super.execute(b)
-        }
+//        // fun1
+//        // Only Accept type of A
+//        fun execute(a: A) {
+//            println("BExecutor execute(a: A) a=${a.a}")
+//        }
+
+//        // fun2
+//        // Only accept type B or subtype
+//        override fun <T : B> execute(b: T) {  // fun2
+//            println("* BExecutor B -> AExecutor super.execute ")
+//            super.execute(b) // just call the parent fn
+//        }
     }
 
+    // We want AExecutor to work on B's, and BExecutor to work on A's
     println()
-    println("With Super AExecutor -> B")
+    println("With Super AExecutor -> B {Super to Sub}")
     with (AExecutor()) {
 //                          // Expected result                               // lambda     fun1          fun2
-//      execute(A())        // Compiler Error, "Required B found A."         // lambda     fun1          fun2
-//        execute(B())        // AExecutor execute(b: B)                          AExec      AExec         AExec
+//      execute(A())        // Compiler Error, "Required B found A."
+        execute(B())        // AExecutor execute(b: B)                          AExec      AExec         AExec
 //      execute((B() as A)) // Compiler Error, "Required B found A."
 //      execute((A() as B)) // compiles, but get type cast error at runtime
     }
 
     println()
-
-    println("With Sub BExecutor -> A")
+    println("With BExecutor -> A {Sub to Super}")
     with (BExecutor()) {
 //                          // Expected result                               // lambda     fun1          fun2
-//        execute(A())        // BExecutor execute(b: B)                          BExec      BExec         BExec
+        execute(A())        // BExecutor execute(b: B)                          BExec      BExec         BExec
         execute(B())        // BExecutor execute(b: B)                          BExec      AExec <-???-> AExec
-//        execute((B() as A)) // BExecutor execute(b: B)                          BExec      BExec         BExec
+        execute((B() as A)) // BExecutor execute(b: B)                          BExec      BExec         BExec
 //      execute((A() as B)) // compiles, but get type cast error at runtime
     }
 
 
 //    println()
 //    println()
+
+    // using contravariance
+    val boxOfA: Box<A> = Box<A>()
+    val boxOfBPtrToBoxOfA: Box<B> = boxOfA
+    val boxOfB: Box<B> = Box<A>()
 
 ////    update(boxOfB, B())
 ////    update(boxOfA, A())
